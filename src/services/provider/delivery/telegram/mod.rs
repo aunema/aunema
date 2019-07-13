47483d8ee3e759088982cfe830626c6914d30e4b
David Ackerman <enfipy@gmail.com>
2019-07-14 00:10:42 +0300

    [error] Add telegram message handle

--- src/services/provider/delivery/telegram/mod.rs
+++ src/services/provider/delivery/telegram/mod.rs
@@ -2,7 +2,7 @@ use crate::config::Config as AppConfig;
 use crate::services::provider::controller::ProviderController;
 
 use carapax::prelude::{
-    Api, App, Context, Handler, HandlerFuture, HandlerResult, Message, SendMessage,
+    Api, App, Context, Handler, HandlerFuture, HandlerResult, Message, SendMessage, Method, MessageData,
 };
 use futures::{
     future::{self, Either},
@@ -34,19 +34,39 @@ impl Handler for ProviderTelegram {
     type Output = HandlerFuture;
 
     fn handle(&self, ctx: &mut Context, msg: Self::Input) -> Self::Output {
-        println!("Got a message");
         let api = ctx.get::<Api>().clone();
-        HandlerFuture::new(future::lazy(move || match msg.get_text() {
-            Some(text) => {
-                // Todo: Handle provider api
-                let chat_id = msg.get_chat_id();
-                let method = SendMessage::new(chat_id, text.data.clone());
-                Either::A(api.execute(method).then(|x| {
-                    println!("Send message result: {:?}\n", x);
-                    Ok(HandlerResult::Continue)
-                }))
-            }
-            None => Either::B(future::ok(HandlerResult::Continue)),
+        HandlerFuture::new(future::lazy(move || {
+            match msg.data.clone() {
+                MessageData::Text(text) => {
+                    let command = text.data.clone();
+                    let method = self.handle_text_message(&api, msg.clone(), command);
+                    api.execute(method)
+                },
+                _ => {
+                    let method = self.handle_unsupported_message(msg.clone());
+                    api.execute(method)
+                }
+            }.then(|_| Ok(HandlerResult::Continue))
         }))
+    }
+}
+
+impl ProviderTelegram {
+    pub fn handle_text_message(&self, api: &Api, msg: Message, command: String) -> impl Method<Response=Message> {
+        match &*command {
+            "/hello" => {
+                let chat_id = msg.get_chat_id();
+                SendMessage::new(chat_id, "Hello there".to_string())
+            }
+            _ => {
+                let chat_id = msg.get_chat_id();
+                SendMessage::new(chat_id, command.clone())
+            }
+        }
+    }
+
+    pub fn handle_unsupported_message(&self, msg: Message) -> impl Method<Response=Message> {
+        let chat_id = msg.get_chat_id();
+        SendMessage::new(chat_id, "Unsupported message".to_string())
     }
 }
